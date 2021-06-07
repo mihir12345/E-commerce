d70dfcf45d00e8582bca25122153f5067da604f4
Mihir <lucasgreat69@gmail.com>
2021-06-07 15:36:02 +0530

    Add files via upload

---
+++ build/precache-manifest.626595d808881427252dd9084d3e38fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acad5c95a90506e7c3ebd98ff2579b2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f326cb100c4f9a6e425e",
+    "url": "/static/js/2.aa701e9a.chunk.js"
+  },
+  {
+    "revision": "70af1bffd51a7281193fa8f9b7319597",
+    "url": "/static/js/2.aa701e9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfc2f36bb38153318680",
+    "url": "/static/js/main.1009dec1.chunk.js"
+  },
+  {
+    "revision": "ed8ed874e871633992a1",
+    "url": "/static/js/runtime-main.a418149b.js"
+  }
+]);
